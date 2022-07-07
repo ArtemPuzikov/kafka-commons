9c6d95aac92654d77bc38355cc39821361e3408b
ArtemPuzikov <Artdmpuzikov@mail.ru>
2022-07-07 21:39:05 +0300

    Initial commit

---
+++ src/main/java/com/puzikov/kafkacommons/KafkaCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.puzikov.kafkacommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCommonsApplication.class, args);
+	}
+
+}
